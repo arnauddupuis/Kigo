387b92635af89c7bd4033993b568f3ca9477c310
arnauddupuis <a.dupuis@infinityperl.org>
2014-02-10 16:03:08 +0100

    WIP

--- kigo.pl
+++ kigo.pl
@@ -13,28 +13,70 @@ use Data::Dumper;
 # global variables (I know).
 my $global_verbose=0;
 my $global_debug=0;
+my @global_valid_types = ('class','controller','api','db_table');
 
 sub verbose {
-	print shift if($global_verbose);
+	print join('',@_) if($global_verbose);
 }
 
 sub debug {
-	print "[debug] " . shift if($global_debug);
+	print "[debug] " . join('',@_) if($global_debug);
 }
 
 sub loadConfig {
 	my $cfg = shift;
-	open(my $fh,'<',shift(@_)) or die $!;
+	my $file = shift;
+	my $extra_tables_keys = shift;
+	debug "\$extra_tables_keys: ", Data::Dumper::Dumper( $extra_tables_keys ), "\n";
+	my @cfg_table_keys = ('templates','generate','members');
+	push @cfg_table_keys, @{ $extra_tables_keys } if(defined($extra_tables_keys) && $extra_tables_keys && ref($extra_tables_keys) eq 'ARRAY');
+	debug "List of all table keys: ", join( ', ', @cfg_table_keys ),"\n";
+	open(my $fh,'<',$file) or die $!;
 	while(<$fh>){
 		chomp;
 		next if(/^[#\/!*]+/);
 		next if(/^\s*$/);
-		verbose "Processing: $_\n";
+		debug "Processing: $_\n";
 		my ($key,$value)= $_ =~ /^([^=]+)\s*=\s*(.+)$/;
 		next unless(defined($value));
 		$cfg->{$key} = $value unless(defined($cfg->{$key}));
 	}
 	close($fh);
+	foreach my $tmp_key ( keys(%{$cfg}) ){
+		# remove uninitialized keys
+		delete $cfg->{$tmp_key} unless( defined( $tmp_key ) && $tmp_key ne '' );
+		debug "$tmp_key : $cfg->{$tmp_key}\n";
+		if( $cfg->{$tmp_key} =~ /;/ ){
+			$cfg->{$tmp_key} = [split(/;/,$cfg->{$tmp_key})];
+		}
+		if( $tmp_key =~ /^([^:]+):([^:\s]+)$/){
+			debug "   (group) $1 (var) $2\n";
+			$cfg->{$1}->{$2} = $cfg->{$tmp_key};
+			if( $cfg->{$1}->{$2} eq $cfg->{$tmp_key}){
+				delete $cfg->{$tmp_key};
+			}
+		}
+	}
+	
+	foreach my $tmp_tbl_key (@cfg_table_keys){
+		if( exists( $cfg->{$tmp_tbl_key} ) ){
+			debug "force change key '$tmp_tbl_key' to table\n";
+			$cfg->{$tmp_tbl_key} = [] unless( defined( $cfg->{$tmp_tbl_key} ) );
+			if( ref($cfg->{$tmp_tbl_key}) ne 'ARRAY'){
+				$cfg->{$tmp_tbl_key} = [$cfg->{$tmp_tbl_key}];
+			}
+		}
+	}
+	
+	# Changing all keys of the form doc:(get|set):var to $cfg->{doc}->{[gs]{1}et}->{var}
+	foreach my $tmp_key ( grep { /^doc:[^:]+:[^:]+$/ } keys(%{$cfg}) ){
+		debug "[DOC] got $tmp_key.\n";
+		my @tmp_tbl_key = split(/:/, $tmp_key);
+		$cfg->{$tmp_tbl_key[0]}->{$tmp_tbl_key[2]}->{$tmp_tbl_key[1]} = $cfg->{$tmp_key};
+		if( exists($cfg->{$tmp_tbl_key[0]}->{$tmp_tbl_key[2]}->{$tmp_tbl_key[1]}) && defined($cfg->{$tmp_tbl_key[0]}->{$tmp_tbl_key[2]}->{$tmp_tbl_key[1]}) && $cfg->{$tmp_tbl_key[0]}->{$tmp_tbl_key[2]}->{$tmp_tbl_key[1]} eq $cfg->{$tmp_key}){
+			delete($cfg->{$tmp_key});
+		}
+	}
 }
 sub loadFile {
 	my $file = shift;
@@ -58,31 +100,32 @@ sub loadTemplate {
 	my $tpl_struct = shift;
 	my $file = shift;
 	my @cfg_table_keys = ('extra_includes','types','use');
-	loadConfig($tpl_struct,$file);
+	debug "Loading template '$file'\n";
+	loadConfig($tpl_struct,$file,\@cfg_table_keys);
 	# TODO: process each template key
 	# NOTE: It looks done...
-	foreach my $tmp_key ( keys(%{$tpl_struct}) ){
-		# remove uninitialized keys
-		delete $tpl_struct->{$tmp_key} unless( defined( $tmp_key ) && $tmp_key ne '' );
-		debug "$tmp_key : $tpl_struct->{$tmp_key}\n";
-		if( $tpl_struct->{$tmp_key} =~ /,/ ){
-			$tpl_struct->{$tmp_key} = [split(/,/,$tpl_struct->{$tmp_key})];
-		}
-		if( $tmp_key =~ /^([^:]+):([^:\s]+)$/){
-			debug "   (group) $1 (var) $2\n";
-			$tpl_struct->{$1}->{$2} = $tpl_struct->{$tmp_key};
-			if( $tpl_struct->{$1}->{$2} eq $tpl_struct->{$tmp_key}){
-				delete $tpl_struct->{$tmp_key};
-			}
-		}
-	}
+# 	foreach my $tmp_key ( keys(%{$tpl_struct}) ){
+# 		# remove uninitialized keys
+# 		delete $tpl_struct->{$tmp_key} unless( defined( $tmp_key ) && $tmp_key ne '' );
+# 		debug "$tmp_key : $tpl_struct->{$tmp_key}\n";
+# 		if( $tpl_struct->{$tmp_key} =~ /,/ ){
+# 			$tpl_struct->{$tmp_key} = [split(/,/,$tpl_struct->{$tmp_key})];
+# 		}
+# 		if( $tmp_key =~ /^([^:]+):([^:\s]+)$/){
+# 			debug "   (group) $1 (var) $2\n";
+# 			$tpl_struct->{$1}->{$2} = $tpl_struct->{$tmp_key};
+# 			if( $tpl_struct->{$1}->{$2} eq $tpl_struct->{$tmp_key}){
+# 				delete $tpl_struct->{$tmp_key};
+# 			}
+# 		}
+# 	}
 	
-	foreach my $tmp_tbl_key (@cfg_table_keys){
-		$tpl_struct->{$tmp_tbl_key} = [] unless( defined( $tpl_struct->{$tmp_tbl_key} ) );
-		if( ref($tpl_struct->{$tmp_tbl_key}) ne 'ARRAY'){
-			$tpl_struct->{$tmp_tbl_key} = [$tpl_struct->{$tmp_tbl_key}];
-		}
-	}
+# 	foreach my $tmp_tbl_key (@cfg_table_keys){
+# 		$tpl_struct->{$tmp_tbl_key} = [] unless( defined( $tpl_struct->{$tmp_tbl_key} ) );
+# 		if( ref($tpl_struct->{$tmp_tbl_key}) ne 'ARRAY'){
+# 			$tpl_struct->{$tmp_tbl_key} = [$tpl_struct->{$tmp_tbl_key}];
+# 		}
+# 	}
 }
 
 sub verifyTemplateSanity {
@@ -103,12 +146,14 @@ sub verifyTemplateSanity {
 			verbose "not ok\n";
 			$status_struct->{'is_sanity_ok'}=0;
 			$status_struct->{'error_string'}="Template must define at least one type to be valid. None found.";
+			return $status_struct;
 		}
 	}
 	else{
 		verbose "not ok\n";
 		$status_struct->{'is_sanity_ok'}=0;
 		$status_struct->{'error_string'}="Template must define at least one type to be valid. None found.";
+		return $status_struct;
 	}
 	
 	
@@ -121,6 +166,7 @@ sub verifyTemplateSanity {
 		$tpl_struct->{$tpl_name}->{'content'}->{'output'}->{'root'} = './generated_'.$tpl_name;
 	}
 	foreach my $type ( @{ $tpl_struct->{$tpl_name}->{'content'}->{'types'} } ){
+		# output directory checking
 		if( defined($tpl_struct->{$tpl_name}->{'content'}->{'output'}->{$type})){
 			verbose "sanity check: output:$type...ok\n";
 		}
@@ -128,7 +174,18 @@ sub verifyTemplateSanity {
 			verbose "sanity check: output:$type...not ok (adding default value: /$type)\n";
 			$tpl_struct->{$tpl_name}->{'content'}->{'output'}->{$type} = "/$type";
 		}
-		push @output_directories, $tpl_struct->{$tpl_name}->{'content'}->{'output'}->{'root'}.$tpl_struct->{$tpl_name}->{'content'}->{'output'}->{$type}
+		push @output_directories, $tpl_struct->{$tpl_name}->{'content'}->{'output'}->{'root'}.$tpl_struct->{$tpl_name}->{'content'}->{'output'}->{$type};
+		
+		# Type consistancy checking
+		if( exists($tpl_struct->{$tpl_name}->{'content'}->{'type'}->{$type}) && grep{ $_ eq $tpl_struct->{$tpl_name}->{'content'}->{'type'}->{$type} } @global_valid_types  ){
+			verbose "sanity check: $type of type '$tpl_struct->{$tpl_name}->{'content'}->{'type'}->{$type}' is a valid one\n";
+		}
+		else{
+			$status_struct->{'is_sanity_ok'}=0;
+			$status_struct->{'error_string'}="$type is declared as a '$tpl_struct->{$tpl_name}->{'content'}->{'type'}->{$type}', wich is not a valid types. Valid types are: ".join(',',@global_valid_types);
+			return $status_struct;
+		}
+		
 	}
 	debug "\@output_directories: ".join(',',@output_directories)."\n";
 	verbose "sanity check: creating output directories.\n";
@@ -142,12 +199,14 @@ sub verifyTemplateSanity {
 			verbose "not ok\n";
 			$status_struct->{'is_sanity_ok'}=0;
 			$status_struct->{'error_string'}="Unable to create the $dir directory (it is possible that other directories could not be created, only the last failure will be shown).";
+			return $status_struct;
 		}
 	}
-	# tables
 	
 	return $status_struct;
 }
+
+## NOTE: Beginning og the main code
 
 my $config={templates_basedir=>'./templates'};
 my $templates = {};
@@ -160,6 +219,8 @@ verbose "Scanning available templates:\n";
 opendir(my $dh, $config->{templates_basedir}) || die;
 my @templates_list = grep { !/^\./ && -d "$config->{templates_basedir}/$_" } readdir $dh;
 closedir $dh;
+
+# For each template verify that it has all required files (at minimum a template.ini file)
 foreach (@templates_list){
 	verbose "- $_";
 	$templates->{$_} = {};
@@ -179,6 +240,7 @@ foreach (@templates_list){
 		else{
 			$templates->{$_}->{is_valid} = 1;
 			$templates->{$_}->{content}={};
+			loadTemplate($templates->{$_}->{content},"$config->{templates_basedir}/$_/template.ini");
 			verbose " (Valid)";
 		}
 	}
@@ -208,26 +270,60 @@ my $code_gen = {
 };
 die "[critical] You MUST pass a template file as argument." unless(defined($ARGV[0]));
 loadConfig($config,$ARGV[0]);
-verbose "Templates: $config->{templates}\n";
-loadTemplate($templates->{$config->{templates}}->{content},"$config->{templates_basedir}/$config->{templates}/template.ini");
 
+if( exists($config->{'templates'}) && ref($config->{'templates'}) eq 'ARRAY' && scalar( @{ $config->{'templates'} } ) >= 1 ){
+	foreach my $code_template ( @{ $config->{'templates'} } ){
+		verbose "Templates: $code_template\n";
+		unless ( $ignore_sanity_check ){
+			foreach my $tmp_tpl ( keys( %{$templates} ) ){
+				if( $templates->{$tmp_tpl}->{'is_valid'} ){
+					my $tpl_sanity;
+					unless(defined($templates->{$tmp_tpl}->{'sanity_check'})){
+						$tpl_sanity = verifyTemplateSanity($templates,$tmp_tpl);
+						debug Data::Dumper::Dumper( $tpl_sanity );
+						$templates->{$tmp_tpl}->{'sanity_check'} = $tpl_sanity;
+					}
+					else{
+						$tpl_sanity = $templates->{$tmp_tpl}->{'sanity_check'};
+					}
+					if( $tpl_sanity->{is_sanity_ok} ){
+						verbose "$tmp_tpl: sanity check...ok\n";
+						if( defined( $templates->{$tmp_tpl}->{'content'}->{'use'} ) ){
+							foreach my $tmp_use_tpl ( @{ $templates->{$tmp_tpl}->{'content'}->{'use'} } ){
+								unless( grep{ $_ eq $tmp_use_tpl } @{ $config->{'templates'} } ){
+									verbose "Adding '$tmp_tpl' to the list of templates to load.\n";
+									push @{ $config->{'templates'} }, $tmp_use_tpl;
+								}
+							}
+						}
+						
+					}
+					else{
+						print "[error] sanity check for template $tmp_tpl...not ok\n\tlast error: $tpl_sanity->{'error_string'}\n";
+					}
+				}
+				else{
+					verbose "$tmp_tpl: template sanity check not executed because the template is already marked as invalid.\n";
+				}
+			}
+		}
+	}
+}
+else {
+	die "[critical] the file $ARGV[0] does not defines any templates to use or was incorrectly parsed.\n";
+}
 
+debug Data::Dumper::Dumper( $config );
 debug Data::Dumper::Dumper( $templates );
 
-unless ( $ignore_sanity_check ){
-	foreach my $tmp_tpl ( keys( %{$templates} ) ){
-		if( $templates->{$tmp_tpl}->{'is_valid'} ){
-			my $tpl_sanity = verifyTemplateSanity($templates,$tmp_tpl);
-			debug Data::Dumper::Dumper( $tpl_sanity );
-			if( $tpl_sanity->{is_sanity_ok} ){
-				verbose "$tmp_tpl: sanity check...ok\n";
-			}
-			else{
-				print "[error] sanity check for template $tmp_tpl...not ok\n\tlast error: $tpl_sanity->{'error_string'}\n";
-			}
-		}
-		else{
-			verbose "$tmp_tpl: template sanity check not executed because the template is already marked as invalid.\n";
-		}
+
+verbose "Checking for templates usage in input file (this will check for templates used by the input file and check each template's dependencies).\n";
+
+# I need to reparse this because now I have the sanity status for all templates.
+foreach my $tmp_tpl ( keys( %{$templates} ) ) {
+	foreach my $used_tpl ( @{ $templates->{$tmp_tpl}->{'content'}->{'use'} } ){
+		die "[critical] Template '$used_tpl' is required by template '$tmp_tpl' but was not found.\n" unless( exists( $templates->{$used_tpl} ) );
+		die "[critical] Template '$used_tpl' is required by template '$tmp_tpl' but is not valid.\n" unless( exists( $templates->{$used_tpl}->{'is_valid'} ) && $templates->{$used_tpl}->{'is_valid'} );
+		die "[critical] Template '$used_tpl' is required by template '$tmp_tpl' but did not pass sanity checks ($templates->{$used_tpl}->{'sanity_check'}->{'error_string'}).\n" unless( exists( $templates->{$used_tpl}->{'sanity_check'} ) && $templates->{$used_tpl}->{'sanity_check'}->{'is_sanity_ok'} );
 	}
 }
